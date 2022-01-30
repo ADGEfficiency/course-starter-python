70f94bbbe254aca86479d147f16a965d6fe80ec8
Adam Green <adam.green@adgefficiency.com>
2022-01-30 19:25:00 +1300

    stay positive

--- inject-aws-netlify.sh
+++ inject-aws-netlify.sh
@@ -1,6 +1,6 @@
 cat > ~/.aws/credentials-netlify <<EOL
 [default]
-region = ap-southeast-2
-aws_secret_access_key = $NETLIFY_AWS_ACCESS_KEY_ID
-aws_access_key_id = $NETLIFY_AWS_SECRET_ACCESS_KEY
+region=ap-southeast-2
+aws_access_key_id=$NETLIFY_AWS_ACCESS_KEY_ID
+aws_secret_access_key=$NETLIFY_AWS_SECRET_ACCESS_KEY
 EOL
