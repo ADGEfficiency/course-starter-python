537170089a5125255d6a13f58bc47c610e5d2a08
Adam Green <adam.green@adgefficiency.com>
2022-02-04 02:08:18 +1300

    remove emoji

--- neuapp/gatsby-config.js
+++ neuapp/gatsby-config.js
@@ -61,7 +61,6 @@ module.exports = {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
-                    `gatsby-remark-emoji`,
                     `gatsby-remark-copy-linked-files`,
                     {
                         resolve: `gatsby-remark-prismjs`,
